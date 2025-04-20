100fc83235f86dc2b51fa06d226e6e8c7c029861
Saurav Kumar <codewizard.saurav@gmail.com>
2025-04-20 23:07:06 +0530

    added fetch notices from databases

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { NewsColumn } from "./ui/home/newsColumn";
 import { MapLocation } from "./ui/home/maps";
 import { MarqueeEffectDoubleExample } from "./ui/home/marquee";
 import { TotalCount } from "./ui/home/detailsCount";
+import { prisma } from "@/lib/prisma";
 
 const details = {
   openedIn: 2020,
@@ -11,12 +12,13 @@ const details = {
 };
 
 export default async function Home() {
+  const res = await prisma.notices.findMany();
   return (
     <>
       <CarouselBody />
       <MarqueeEffectDoubleExample />
       <div className="flex flex-col md:flex-row items-center bg-teal-50">
-        <NewsColumn />
+        <NewsColumn notices={res} />
         <MapLocation />
       </div>
       {/* <MessageBar /> */}
--- app/ui/home/newsColumn.tsx
+++ app/ui/home/newsColumn.tsx
@@ -1,70 +1,10 @@
-// "use client";
-
-// import { useRef, useEffect, useState } from "react";
-// import { DotFilledIcon } from "@radix-ui/react-icons";
-
-// const arr = new Array(10).fill(0).map((_, i) => i + 1);
-
-// export const NewsColumn = () => {
-//   const scrollRef = useRef<HTMLDivElement>(null);
-//   const [isPaused, setIsPaused] = useState(false);
-
-//   useEffect(() => {
-//     const scrollDiv = scrollRef.current;
-//     if (!scrollDiv) return;
-
-//     const scrollSpeed = 1; // Adjust for faster/slower scrolling
-//     let scrollInterval: NodeJS.Timeout;
-
-//     const startScrolling = () => {
-//       scrollInterval = setInterval(() => {
-//         if (!isPaused) {
-//           if (
-//             scrollDiv.scrollTop + scrollDiv.clientHeight >=
-//             scrollDiv.scrollHeight
-//           ) {
-//             scrollDiv.scrollTop = 0; // Reset to the top when reaching the bottom
-//           }
-//           scrollDiv.scrollTop += scrollSpeed;
-//         }
-//       }, 50);
-//     };
-
-//     startScrolling();
-
-//     return () => clearInterval(scrollInterval); // Cleanup on unmount
-//   }, [isPaused]); // Depend on isPaused state
-//   return (
-//     <div
-//       className="container bg-white w-[90%] mx-auto md:mx-5 my-5 md:w-[50%]  h-[50vh] md:h-[30vh] xl:h-[50vh] p-4 pb-1 border  rounded shadow"
-//       onMouseEnter={() => setIsPaused(true)}
-//       onMouseLeave={() => setIsPaused(false)}
-//     >
-//       <h1 className="text-lg text-center font-semibold">News And Events</h1>
-//       <div className="container p-3 overflow-hidden">
-//         <p>Welcome to AV School of Nursing Website</p>
-//         <hr />
-//         <div ref={scrollRef} className="h-48 overflow-y-hidden">
-//           {arr.map((x) => (
-//             <p key={x} className="p-2 cursor-pointer text-blue-500">
-//               <DotFilledIcon className="inline" color="black" />
-//               &nbsp; News And Events {x}
-//             </p>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
 "use client";
 
 import { useRef, useEffect, useState } from "react";
 import { DotFilledIcon } from "@radix-ui/react-icons";
+import { Notices } from "@/prisma/generated/client";
 
-const arr = new Array(10).fill(0).map((_, i) => i + 1);
-
-export const NewsColumn = () => {
+export const NewsColumn = ({ notices }: { notices: Notices[] }) => {
   const scrollRef = useRef<HTMLDivElement>(null);
   const [isPaused, setIsPaused] = useState(false);
 
@@ -111,13 +51,13 @@ export const NewsColumn = () => {
           ref={scrollRef}
           className="h-48 md:h-[13.5rem] lg:h-[20rem] xl:h-[13rem] overflow-y-hidden space-y-1"
         >
-          {arr.map((x) => (
+          {notices.map((notice) => (
             <p
-              key={x}
+              key={notice.id}
               className="p-1 text-teal-800 hover:text-teal-600  transition cursor-pointer"
             >
               <DotFilledIcon className="inline mr-1" color="black" />
-              News And Events {x}
+              {notice.notice}
             </p>
           ))}
         </div>
